86af43bd2c44eec785e3536a8beeffebe6b61a3b
sateeshpuvvalla <satishpuvvalla@gmail.com>
2018-05-18 17:06:37 +0530

    hello eureka2 commit

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -11,5 +11,8 @@ public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 		System.out.println("Hello Sateesh");
+		System.out.println("Hello eureka");
+		System.out.println("Hello eureka1");
+		System.out.println("Hello eureka2");
 	}
 }
